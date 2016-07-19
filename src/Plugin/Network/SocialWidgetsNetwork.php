5c1faebd72676785fa1a2734cb9f81b8e5713f84
Getulio Sánchez <valentin2507@gmail.com>
2016-07-19 19:00:58 -0400

    Add a Social Widgets Network Plugin class

---
+++ src/Plugin/Network/SocialWidgetsNetwork.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Drupal\social_widgets\Plugin\Network;
+
+use Drupal\social_api\Plugin\NetworkBase;
+
+/**
+ * Defines a Network Plugin for Social Widgets.
+ */
+class SocialWidgetsNetwork extends NetworkBase implements SocialWidgetsNetworkInterface {
+
+  protected function initSdk(){}
+}
--- src/Plugin/Network/SocialWidgetsNetworkInterface.php
+++ src/Plugin/Network/SocialWidgetsNetworkInterface.php
@@ -5,6 +5,6 @@ namespace Drupal\social_widgets\Plugin\Network;
 use Drupal\social_api\Plugin\NetworkInterface;
 
 /**
- * Defines an interface for Social Widgets Network
+ * Defines an interface for Social Widgets Network.
  */
 interface SocialWidgetsNetworkInterface extends NetworkInterface {}
